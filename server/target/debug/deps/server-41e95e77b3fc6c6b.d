5e3a9ecd72e4e71614978fc565d04ac8f8257764
Anhelina <krukanhelina06@gmail.com>
2025-11-25 11:07:21 +0100

    cargo new

---
+++ server/target/debug/deps/server-41e95e77b3fc6c6b.d
@@ -0,0 +1,5 @@
+/Users/anhelina/Desktop/verity/server/target/debug/deps/server-41e95e77b3fc6c6b.d: src/main.rs
+
+/Users/anhelina/Desktop/verity/server/target/debug/deps/libserver-41e95e77b3fc6c6b.rmeta: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/deps/server-9554645058c56452.d
@@ -0,0 +1,5 @@
+/Users/anhelina/Desktop/verity/server/target/debug/deps/server-9554645058c56452.d: src/main.rs
+
+/Users/anhelina/Desktop/verity/server/target/debug/deps/libserver-9554645058c56452.rmeta: src/main.rs
+
+src/main.rs:
